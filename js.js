003acc57448cb62cc39a37538e9fd039e10f6b71
Anthony <42196548+Sheeplet1@users.noreply.github.com>
2023-02-09 14:26:56 +1100

    centered grid container and set height and width

--- js.js
+++ js.js
@@ -1,6 +1,29 @@
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    div.classList.add("grid-item")
-    div.textContent = i + 1
-    document.querySelector(".grid-container").appendChild(div)
-}
+const gridSizeInput = document.querySelector("#grid-size");
+const createGrid = document.querySelector("#create-grid");
+const gridContainer = document.querySelector(".grid-container");
+
+// create grid button function
+createGrid.addEventListener("click", () => {
+    // reset grid
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild)
+    }
+
+    // get grid input from user input as integer
+    const gridSize = parseInt(gridSizeInput.value);
+    
+    // set grid template
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+    // create grid 
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const div = document.createElement("div");
+        div.classList.add("grid-item");
+        div.textContent = i + 1;
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "yellow";
+        })
+        gridContainer.appendChild(div);
+    }
+})
--- planning.md
+++ planning.md
@@ -7,6 +7,17 @@ Description:
 - 16x16 grid of square divs
   - can use borders to separate each square
 
+What we need:
+
+Functions:
+
+- createGrid
+  - Create nxn grid of squares
+    - loop through n x n times and create div element that represents the square
+  - in those squares, add event listeners that add hover effect on mouseover
+  to change background colour of the squares
+- resetGrid
+
 ## Stage 2
 
 - hover effect that changes the colour of the square that your mouse passes
